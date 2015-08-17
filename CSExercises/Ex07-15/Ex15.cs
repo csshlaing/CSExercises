8f22b9675d6650dabd34bffd808cf2a5494594b5
csshlaing <csshlaing01@gmail.com>
2015-08-17 16:50:51 +0800

    Done Ex20

--- CSExercises/Ex07-15/Ex15.cs
+++ CSExercises/Ex07-15/Ex15.cs
@@ -1,10 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 namespace CSExercises
 {
     public class Ex15
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please enter three digits");
+            double dblValue = Convert.ToDouble(Console.ReadLine());
+
+            if(dblValue > 100 && dblValue < 999)
+            {
+
+                double dblV1, dblV2, dblV3;
+
+                dblV1 = Math.Floor(dblValue / 100);
+                Console.WriteLine("{0}", dblV1);    //print just for checking
+
+                dblV2 = Math.Floor((dblValue - (dblV1 * 100)) / 10);
+                Console.WriteLine("{0}", dblV2);    //print just for checking
+
+                dblV3 = Math.Floor(dblValue % 10);
+                Console.WriteLine("{0}", dblV3);    //print just for checking
+
+                double dblArmstrongNumber = ((dblV1 * dblV1 * dblV1) + (dblV2 * dblV2 * dblV2) + (dblV3 * dblV3 * dblV3));
+                if (dblArmstrongNumber == dblValue)
+                {
+                    Console.WriteLine("Your Number, {0} is Armstrong Number.", dblValue);
+                }
+                else
+                {
+                    Console.WriteLine("Not Armstrong Number! Please try again.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter between 100 to 999!");
+            }                       
         }
     }
-}
+} 
--- CSExercises/Ex16-20/Ex20.cs
+++ CSExercises/Ex16-20/Ex20.cs
@@ -1,10 +1,50 @@
-﻿namespace CSExercises
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace CSExercises
 {
     public class Ex20
     {
+        static int read()
+        {
+            return Convert.ToInt16(Console.ReadLine());           
+        }
+
+        public static double GrandTotal(int qtyTV, int qtyDVD, int qtyMP3)
+        {
+            if ((qtyTV * 900 + qtyDVD * 500) < 5000)
+            {
+                double totalAmt = (qtyTV * 900) + (qtyDVD * 500) + (qtyMP3 * 700);
+                return totalAmt;
+            }
+            else
+            {
+                if ((qtyTV * 900 + qtyDVD * 500) < 10000)
+                {
+                    double totalAmt = ((qtyTV * 900 + qtyDVD * 500) * 0.9);
+                    return (totalAmt + (qtyMP3 * 700));
+                }
+                else
+                {
+                    double totalAmt = ((qtyTV * 900 + qtyDVD * 500) * 0.85);
+                    return (totalAmt + (qtyMP3 * 700));
+                }
+            }
+        }        
         public static void Main(string[] args)
         {
-            
+
+            int qtyTV, qtyDVD, qtyMP3;
+            Console.WriteLine("Enter quantity for TV :");
+            qtyTV = read();
+            Console.WriteLine("Enter quantity for DVD :"); 
+            qtyDVD = read();
+            Console.WriteLine("Enter quantity for MP3 :");
+            qtyMP3 = read();
+            Console.WriteLine("The Grand Total Price for your order is {0}", GrandTotal(qtyTV, qtyDVD, qtyMP3));
+
         }
     }
 }
